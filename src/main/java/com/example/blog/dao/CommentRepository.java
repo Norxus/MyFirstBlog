9f0760f9faa2222cc4ba3d7ff191078cf524be03
Norxus <2232175866@qq.com>
2021-01-28 22:11:27 +0800

    第一次上传博客项目

---
+++ src/main/java/com/example/blog/dao/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.blog.dao;
+
+import com.example.blog.po.Comment;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by limi on 2017/10/22.
+ */
+public interface CommentRepository extends JpaRepository<Comment,Long>{
+
+
+    List<Comment> findByBlogIdAndParentCommentNull(Long blogId, Sort sort);
+}
